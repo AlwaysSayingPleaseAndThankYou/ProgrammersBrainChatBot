1ff1ab74358850d12138ab5bee5d65e384137547
AlwaysSayingPleaseAndThankYou <aengus.crowley@gmail.com>
2023-09-08 22:35:02 -0700

    switched to scanning the book as pictures

--- brain.py
+++ brain.py
@@ -1,4 +1,5 @@
-from langchain.document_loaders import PyPDFLoader, BSHTMLLoader
+import pytesseract
+from langchain.document_loaders import PyPDFLoader, BSHTMLLoader, TextLoader, UnstructuredPDFLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.indexes import VectorstoreIndexCreator
@@ -13,18 +14,55 @@ from langchain import LLMChain
 from langchain.memory import ConversationBufferWindowMemory
 from langchain.vectorstores import Chroma
 from langchain.chains import RetrievalQA
+from pytesseract import image_to_string
+import pypdfium2 as pdfium
+from io import BytesIO
+from PIL import Image
+from llama_index import SimpleDirectoryReader, VectorStoreIndex
+from llama_index.node_parser import SimpleNodeParser
+from llama_index.retrievers import VectorIndexRetriever
+from llama_index.query_engine import RetrieverQueryEngine
+from llama_index.indices.postprocessor import SimilarityPostprocessor
 
 
-def get_html_content(url: str) -> str:
-    """
-    Fetch the HTML content of a given URL using the requests library.
+def convert_pdf_to_images(file_path, scale=300 / 72):
+    ## 1/ This is used to load local files instead of fetching from url
 
-    :param url: The URL of the webpage.
-    :return: The HTML content of the webpage.
-    """
-    response = requests.get(url)
-    response.raise_for_status()  # Raise an exception for HTTP errors
-    return response.text
+    pdf_file = pdfium.PdfDocument(file_path)
+
+    page_indices = [i for i in range(len(pdf_file))]
+
+    renderer = pdf_file.render(
+        pdfium.PdfBitmap.to_pil,
+        page_indices=page_indices,
+        scale=scale,
+    )
+
+    final_images = []
+
+    for i, image in zip(page_indices, renderer):
+        image_byte_array = BytesIO()
+        image.save(image_byte_array, format='jpeg', optimize=True)
+        image_byte_array = image_byte_array.getvalue()
+        final_images.append(dict({i: image_byte_array}))
+
+    return final_images
+
+
+def extract_text_from_img(list_dict_final_images):
+    image_list = [list(data.values())[0] for data in list_dict_final_images]
+    image_content = []
+
+    for index, image_bytes in enumerate(image_list):
+        try:
+            image = Image.open(BytesIO(image_bytes))
+            raw_text = str(image_to_string(image))
+            image_content.append(raw_text)
+            print(raw_text)
+        except pytesseract.TesseractError as e:
+            print(e)
+
+    return "\n".join(image_content)
 
 
 llm = ChatOpenAI(temperature=0.2, model="gpt-3.5-turbo-0613")
@@ -34,17 +72,36 @@ embeddings_model = OpenAIEmbeddings()
 
 text_splitter = CharacterTextSplitter(
     separator='\n',
-    chunk_size=10000,
-    chunk_overlap=2000,
+    chunk_size=1024,
+    chunk_overlap=20,
     length_function=len,
     add_start_index=True,
 )
 book_path = Path('temp', 'brain.pdf')
-loader = PyPDFLoader(str(book_path.absolute()))
+book_text_path = Path('temp', 'brain.txt')
+
+if not book_text_path.exists():
+    images = convert_pdf_to_images(book_path)
+    book_text = extract_text_from_img(images)
+    book_text = book_text.replace('©Manning Publications Co. To comment go to liveBook', '')
+    with book_text_path.open('w+') as f:
+        f.write(book_text)
+loader = TextLoader(str(book_text_path.absolute()))
+# pdfloader = UnstructuredPDFLoader(str(book_path.absolute()))
 # index = VectorstoreIndexCreator().from_loaders([loader])
 documents = loader.load()
 texts = text_splitter.split_documents(documents)
+# texts = text_splitter.split_documents(pdfloader.load())
 chroma = Chroma.from_documents(texts, embeddings_model, collection_name='the-programmers-brain')
+# llama vector store
+# reader = SimpleDirectoryReader(
+#     input_files=[str(book_text_path)]
+# )
+# llama_data = reader.load_data()
+# node_parser = SimpleNodeParser.from_defaults()
+# nodes = node_parser.get_nodes_from_documents(llama_data)
+# vector_index = VectorStoreIndex(nodes)
+# retriever = index.as_query_engine()
 index = RetrievalQA.from_chain_type(llm=llm, chain_type='stuff', retriever=chroma.as_retriever())
 tools = [
     Tool(name='search',
